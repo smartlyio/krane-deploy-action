d01b945319ac156fc021b3279ede8e7663129eb8
Simon Jagoe <simon.jagoe@smartly.io>
2020-08-18 16:11:59 +0300

    Rebuild for release

--- dist/index.js
+++ dist/index.js
@@ -34,7 +34,7 @@ module.exports =
 /******/ 	// the startup function
 /******/ 	function startup() {
 /******/ 		// Load entry module and return exports
-/******/ 		return __webpack_require__(198);
+/******/ 		return __webpack_require__(526);
 /******/ 	};
 /******/
 /******/ 	// run startup
@@ -975,43 +975,22 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const core = __importStar(__webpack_require__(470));
 const kube_1 = __webpack_require__(747);
 const krane_1 = __webpack_require__(739);
-function run() {
+function main(currentSha, dockerRegistry, kubernetesServerRaw, kubernetesContext, kubernetesClusterDomain, kubernetesNamespace, kraneTemplateDir, kraneSelector, kranePath, extraBindingsRaw) {
     return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const currentSha = core.getInput('currentSha');
-            const dockerRegistry = core.getInput('dockerRegistry');
-            let kubernetesServer = core.getInput('kubernetesServer');
-            const kubernetesContext = core.getInput('kubernetesContext');
-            const kubernetesClusterDomain = core.getInput('kubernetesClusterDomain');
-            const kubernetesNamespace = core.getInput('kubernetesNamespace');
-            const kraneTemplateDir = core.getInput('kubernetesTemplateDir');
-            const kraneSelector = core.getInput('kraneSelector');
-            const kranePath = core.getInput('kranePath');
-            const extraBindings = JSON.parse(core.getInput('extraBindings'));
-            if (kubernetesServer === '') {
-                kubernetesServer = `https://${kubernetesClusterDomain}:6443`;
-            }
-            yield kube_1.configureKube(kubernetesServer, kubernetesContext, kubernetesNamespace);
-            const renderedTemplates = yield krane_1.render(kranePath, currentSha, dockerRegistry, kubernetesClusterDomain, kraneTemplateDir, extraBindings);
-            yield krane_1.deploy(kranePath, kubernetesContext, kubernetesNamespace, kraneSelector, kraneTemplateDir, renderedTemplates);
+        const extraBindings = JSON.parse(extraBindingsRaw);
+        let kubernetesServer = kubernetesServerRaw;
+        if (kubernetesServer === '') {
+            kubernetesServer = `https://${kubernetesClusterDomain}:6443`;
         }
-        catch (error) {
-            core.setFailed(error.message);
-        }
+        yield kube_1.configureKube(kubernetesServer, kubernetesContext, kubernetesNamespace);
+        const renderedTemplates = yield krane_1.render(kranePath, currentSha, dockerRegistry, kubernetesClusterDomain, kraneTemplateDir, extraBindings);
+        yield krane_1.deploy(kranePath, kubernetesContext, kubernetesNamespace, kraneSelector, kraneTemplateDir, renderedTemplates);
     });
 }
-run();
+exports.main = main;
 
 
 /***/ }),
@@ -1295,6 +1274,55 @@ function getState(name) {
 }
 exports.getState = getState;
 //# sourceMappingURL=core.js.map
+
+/***/ }),
+
+/***/ 526:
+/***/ (function(__unusedmodule, exports, __webpack_require__) {
+
+"use strict";
+
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const core = __importStar(__webpack_require__(470));
+const main_1 = __webpack_require__(198);
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const currentSha = core.getInput('currentSha');
+            const dockerRegistry = core.getInput('dockerRegistry');
+            const kubernetesServer = core.getInput('kubernetesServer');
+            const kubernetesContext = core.getInput('kubernetesContext');
+            const kubernetesClusterDomain = core.getInput('kubernetesClusterDomain');
+            const kubernetesNamespace = core.getInput('kubernetesNamespace');
+            const kraneTemplateDir = core.getInput('kubernetesTemplateDir');
+            const kraneSelector = core.getInput('kraneSelector');
+            const kranePath = core.getInput('kranePath');
+            const extraBindings = core.getInput('extraBindings');
+            yield main_1.main(currentSha, dockerRegistry, kubernetesServer, kubernetesContext, kubernetesClusterDomain, kubernetesNamespace, kraneTemplateDir, kraneSelector, kranePath, extraBindings);
+        }
+        catch (error) {
+            core.setFailed(error.message);
+        }
+    });
+}
+run();
+
 
 /***/ }),
 
