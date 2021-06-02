305eb1568f93def6dc63660a8222daf0578b80ba
Simon Jagoe <simon.jagoe@smartly.io>
2021-06-02 16:07:04 +0300

    Add basic yaml stream formatting tests

---
+++ test/test-annotations.test.ts
@@ -0,0 +1,66 @@
+import YAML from 'yaml'
+import {
+  addAnnotation,
+  addAnnotationToDocument,
+  getChangeCauseAnnotation
+} from '../src/annotations'
+
+const BASIC_DOCUMENT = `
+kind: Deployment
+metadata:
+  name: test-deployment
+  labels:
+    krane: "yes"
+`
+
+
+const DOCUMENT_STREAM = `
+---
+${BASIC_DOCUMENT}
+---
+kind: Service
+metadata:
+  name: a-service
+  labels:
+    application: web
+---
+kind: DaemonSet
+metadata:
+  name: a-service
+  annotations:
+    colour: blue
+`
+
+describe('add annotation', () => {
+  test('addAnnotationToDocument', () => {
+    const document: YAML.Document = YAML.parseDocument(BASIC_DOCUMENT)
+    const annotation = 'annotation-name'
+    const value = 'annotation-value'
+    addAnnotationToDocument(document, annotation, value)
+
+    expect(document.get('metadata').has('annotations')).toEqual(true)
+    const annotations = document.get('metadata').get('annotations')
+    expect(annotations.has(annotation)).toEqual(true)
+    expect(annotations.get(annotation)).toEqual(value)
+  })
+
+  test('addAnnotation', () => {
+    const annotation = 'annotation-name'
+    const value = 'annotation-value'
+    const updatedDocuments = addAnnotation(
+      DOCUMENT_STREAM,
+      annotation,
+      value
+    )
+
+    const documents = YAML.parseAllDocuments(updatedDocuments).map((d) => d.toJSON())
+
+    const [deployment, service, daemonset] = documents
+    expect('annotations' in service.metadata).toEqual(false)
+    expect(deployment.metadata.annotations).toEqual({[annotation]: value})
+    expect(daemonset.metadata.annotations).toEqual({
+      [annotation]: value,
+      colour: 'blue'
+    })
+  })
+})
