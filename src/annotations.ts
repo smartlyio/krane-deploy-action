d0636b5635fdbcae36408c5bcd9e6d00dd277d86
Simon Jagoe <simon.jagoe@smartly.io>
2021-06-02 13:39:19 +0300

    Add functions to automatically annontate deployments with change-cause

---
+++ src/annotations.ts
@@ -0,0 +1,60 @@
+import YAML from 'yaml'
+
+function addAnnotationToDocument(
+  document: YAML.Document,
+  annotationName: string,
+  annotationValue: string
+): void {
+  const kind = document.get('kind').toLowerCase()
+  // change-cause (or rather, kubectl rollout history) is only valid
+  // for deployments, daemonsets and statefulsets.
+  if (!['deployment', 'daemonset', 'statefulset'].includes(kind)) {
+    return
+  }
+  if (!document.has('metadata')) {
+    document.set('metadata', YAML.createNode({}))
+  }
+  const metadata: YAML.YamlMap = document.get('metadata')
+  if (!metadata.has('annotations')) {
+    metadata.set('annotations', YAML.createNode({}))
+  }
+  const annotations: YAML.YamlMap = metadata.get('annotations')
+  if (!annotations.has(annotationName)) {
+    annotations.set(annotationName, annotationValue)
+  }
+}
+
+export function addAnnotation(
+  manifestStream: string,
+  annotationName: string,
+  annotationValue: string
+): string {
+  const yamlDocuments = YAML.parseAllDocuments(manifestStream)
+  let updatedManifests = ''
+  for (const document of yamlDocuments) {
+    addAnnotationToDocument(document, annotationName, annotationValue)
+    const updatedDocument: string = YAML.stringify(document)
+    updatedManifests = `${updatedManifests}---
+${updatedDocument}`
+  }
+  return updatedManifests
+}
+
+export function getChangeCauseAnnotation(
+  currentSha: string,
+  bindings: Record<string, string>
+): string {
+  const parts = ['type=krane']
+  if (bindings.deployer) {
+    parts.push(`deployer=${bindings.deployer}`)
+  }
+  const revision = bindings.deploy_revision
+    ? bindings.deploy_revision
+    : currentSha
+  parts.push(`revision=${revision}`)
+  // Quick and dirty date format similar to what ruby produces with Time.now.getutc
+  const now = new Date().toISOString().replace('T', ' ').replace('Z', ' UTC')
+  parts.push(`at=${now}`)
+  parts.push('annotated-automatically=true')
+  return parts.join(',')
+}
